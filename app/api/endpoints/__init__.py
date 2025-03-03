65c5f8ce670ac4bf5f2535f639a8f56fba776865
gustavolafz <gustavolimaaferreira@gmail.com>
2025-03-03 15:30:18 -0300

    ROTA DE VISUALIZACAO DE IMOVEL POR ID IMPLEMENTADA

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -3,8 +3,10 @@
 
 from flask import Blueprint
 from app.api.endpoints.view_imoveis import view_imoveis_bp
+from app.api.endpoints.view_imoveis_from_id import view_imovel_by_id_bp
 
 api_bp = Blueprint('api', __name__)
 
 # Registra todas as rotas disponíveis
 api_bp.register_blueprint(view_imoveis_bp, url_prefix='/imoveis')
+api_bp.register_blueprint(view_imovel_by_id_bp, url_prefix='/imoveis')
---
+++ app/api/endpoints/view_imoveis_from_id.py
@@ -0,0 +1,37 @@
+#  DESC: Endpoint para visualizar um imóvel a partir de um ID
+# app/api/endpoints/view_imoveis_from_id.py
+
+from flask import Blueprint, jsonify
+from app.db.database import connect_db  # Importando a função de conexão
+
+view_imovel_by_id_bp = Blueprint('view_imovel_by_id', __name__)  # Novo nome para o Blueprint
+
+@view_imovel_by_id_bp.route('/view_imoveis_from_id/<int:id>', methods=['GET'])
+def view_imoveis_from_id(id):
+    conn = connect_db()  # Conectando ao banco de dados
+
+    if conn is None:
+        return jsonify({'erro': 'Erro ao conectar ao banco de dados'}), 500
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM imoveis WHERE ID = %s", (id,))
+    results = cursor.fetchall()
+
+    if not results:
+        return jsonify({'erro': 'Nenhum imóvel encontrado.'}), 404
+
+    imoveis = []
+    for imovel in results:
+        imoveis.append({
+            'id': imovel[0],
+            'logradouro': imovel[1],
+            'tipo_logradouro': imovel[2],
+            'bairro': imovel[3],
+            'cidade': imovel[4],
+            'cep': imovel[5],
+            'tipo': imovel[6],
+            'valor': imovel[7],
+            'data_aquisicao': imovel[8],
+        })
+
+    return jsonify({'imoveis': imoveis}), 200
